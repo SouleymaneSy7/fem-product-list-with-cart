1f8a830d7fa6d8948dd5a0c48c6db80bdcd26f13
Souleymane Sy <souleymanesyservices@gmail.com>
2024-08-30 19:59:07 +0200

    feat: add the main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import Carts from "./Carts";
+import Products from "./Products";
+
+const Main = () => {
+  return (
+    <main className="main">
+      <Products />
+      <Carts />
+    </main>
+  );
+};
+
+export default Main;
