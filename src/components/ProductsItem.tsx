f42b41128935652a107915421bcbb6b5f72b9101
Souleymane Sy <souleymanesyservices@gmail.com>
2024-08-30 20:01:38 +0200

    feat: add the productsItem component

---
+++ src/components/ProductsItem.tsx
@@ -0,0 +1,45 @@
+type ProductsItemPropsType = {
+  id: number;
+  image: string;
+  name: string;
+  category: string;
+  price: number;
+};
+
+const ProductsItem = ({
+  // id,
+  image,
+  name,
+  category,
+  price,
+}: ProductsItemPropsType) => {
+  const count = 11;
+
+  return (
+    <div className="product__card">
+      <div className="product__card__top">
+        <img className="card__img" src={image} alt={`${name} food image`} />
+
+        {count >= 1 ? (
+          <div className="card__btn__flex">
+            <button type="button" className="card--btn--minus">-</button>
+            <p className="card--price">{count}</p>
+            <button type="button" className="card--btn--plus">+</button>
+          </div>
+        ) : (
+          <button type="button" className="card--btn--add-to">
+            add to the cart
+          </button>
+        )}
+      </div>
+
+      <div className="product__card__bottom">
+        <p className="card__category">{category}</p>
+        <h2 className="card__name">{name}</h2>
+        <p className="card__price">{price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsItem;
