4556c6ee5202028e41dc0c1095d229003a5f8cc1
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-02 15:08:27 +0200

    feat: add the productsItem to use the zustand store data

--- src/components/ProductsItem.tsx
+++ src/components/ProductsItem.tsx
@@ -1,48 +1,70 @@
+import useCartStore from "@/store/productStore";
 import { formatNumber } from "@/utils/currencyFormat";
 
 type ProductsItemPropsType = {
-  id: number;
-  image: string;
-  name: string;
-  category: string;
-  price: number;
+  product: {
+    id: number;
+    image: string;
+    name: string;
+    category: string;
+    price: number;
+  };
 };
 
-const ProductsItem = ({
-  // id,
-  image,
-  name,
-  category,
-  price,
-}: ProductsItemPropsType) => {
-  const count = 11;
+const ProductsItem = ({ product }: ProductsItemPropsType) => {
+  const addCartItem = useCartStore((state) => state.addCartItem);
+  const cartQuantity = useCartStore((state) => state.getCartItem(product.id));
+  const increaseCartQuantity = useCartStore((state) => state.increaseCartItem);
+  const decreaseCartQuantity = useCartStore((state) => state.decreaseCartItem);
 
   return (
     <div className="product__card">
       <div className="product__card__top">
-        <img className="card__img" src={image} alt={`${name} food image`} />
+        <img
+          className="card__img"
+          src={product.image}
+          alt={`${product.name} food image`}
+        />
 
-        {count >= 1 ? (
+        {cartQuantity === 0 ? (
+          <button
+            type="button"
+            className="card--btn--add-to"
+            onClick={() => {
+              addCartItem(product);
+            }}
+          >
+            add to the cart
+          </button>
+        ) : (
           <div className="card__btn__flex">
-            <button type="button" className="card--btn--minus">
+            <button
+              type="button"
+              className="card--btn--minus"
+              onClick={() => {
+                decreaseCartQuantity(product.id);
+              }}
+            >
               -
             </button>
-            <p className="card--price">{count}</p>
-            <button type="button" className="card--btn--plus">
+            <p className="card--price">{cartQuantity}</p>
+            <button
+              type="button"
+              className="card--btn--plus"
+              onClick={() => {
+                increaseCartQuantity(product.id);
+              }}
+            >
               +
             </button>
           </div>
-        ) : (
-          <button type="button" className="card--btn--add-to">
-            add to the cart
-          </button>
         )}
       </div>
 
       <div className="product__card__bottom">
-        <p className="card__category">{category}</p>
-        <h2 className="card__name">{name}</h2>
-        <p className="card__price">{formatNumber(price)}</p>
+        <p className="card__category">{product.category}</p>
+        <h2 className="card__name">{product.name}</h2>
+        <p className="card__price">{formatNumber(product.price)}</p>
       </div>
     </div>
   );
