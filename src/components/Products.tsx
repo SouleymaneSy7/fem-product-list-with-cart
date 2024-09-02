ec7fc73c6d5da4d362cf868b3370db2f6bdf9cd4
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-02 15:07:09 +0200

    feat: add the product structure & logic

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -7,14 +7,7 @@ const Products = () => {
       <h1 className="product__title">Desserts</h1>
 
       {productData.map((product) => (
-        <ProductsItem
-          key={product.id}
-          id={product.id}
-          image={product.image}
-          name={product.name}
-          category={product.category}
-          price={product.price}
-        />
+        <ProductsItem key={product.id} product={product} />
       ))}
     </section>
   );
