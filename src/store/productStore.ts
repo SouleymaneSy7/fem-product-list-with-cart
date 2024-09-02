b77ccec70a1ffa869e8ed44a3f5de33a989b5107
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-02 15:04:15 +0200

    feat: add the add, get, increase, decrease, remove & reset function for the store

--- src/store/productStore.ts
+++ src/store/productStore.ts
@@ -32,6 +32,73 @@ const initialState: StateType = {
 
 const useCartStore = create<StateType & ActionsType>()((set, get) => ({
   ...initialState,
+
+  addCartItem: (product) => {
+    const findCartItem = get().cartItems.find((item) => item.id === product.id);
+
+    if (findCartItem) {
+      if (typeof findCartItem.cartQuantity === "number") {
+        findCartItem.cartQuantity++;
+      }
+
+      set({ cartItems: [...get().cartItems] });
+    } else {
+      set({ cartItems: [...get().cartItems, { ...product, cartQuantity: 1 }] });
+    }
+  },
+
+  getCartItem: (id) => {
+    return get().cartItems.find((item) => item.id === id)?.cartQuantity || 0;
+  },
+
+  increaseCartItem: (id: number) => {
+    const findCartItem = get().cartItems.find((item) => item.id === id);
+
+    if (findCartItem) {
+      if (typeof findCartItem.cartQuantity === "number") {
+        findCartItem.cartQuantity++;
+      }
+
+      set({ cartItems: [...get().cartItems] });
+    }
+  },
+
+  decreaseCartItem: (id: number) => {
+    const findCartItem = get().cartItems.find((item) => item.id === id);
+
+    if (findCartItem) {
+      if (typeof findCartItem.cartQuantity === "number") {
+        if (findCartItem.cartQuantity === 1) {
+          const deletedCartItems = get().cartItems.filter(
+            (item) => item.id !== id
+          );
+
+          set({ cartItems: deletedCartItems });
+        } else {
+          findCartItem.cartQuantity--;
+          set({ cartItems: [...get().cartItems] });
+        }
+      }
+    }
+  },
+
+  removeCartItem: (id: number) => {
+    const findCartItem = get().cartItems.find((item) => item.id === id);
+
+    if (findCartItem) {
+      if (typeof findCartItem.cartQuantity === "number") {
+        const deletedCartItems = get().cartItems.filter(
+          (item) => item.id !== id
+        );
+
+        set({ cartItems: deletedCartItems });
+      }
+    }
+  },
+
+  resetCartItem: () => {
+    set(initialState);
+  },
 }));
 
 export default useCartStore;
