965bbd58f5e5a0a040bc27e99c679864cc236179
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-04 00:57:52 +0200

    feat: update the product component structure

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,9 +6,11 @@ const Products = () => {
     <section className="product-container">
       <h1 className="product__title">Desserts</h1>
 
-      {productData.map((product) => (
-        <ProductsItem key={product.id} product={product} />
-      ))}
+      <div className="product__flex">
+        {productData.map((product) => (
+          <ProductsItem key={product.id} product={product} />
+        ))}
+      </div>
     </section>
   );
 };
