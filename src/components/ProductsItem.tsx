7a312516e8484b7c1dffa8eeeb67a1dcca540ce3
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-04 00:59:36 +0200

    feat: update the productsItem structure

--- src/components/ProductsItem.tsx
+++ src/components/ProductsItem.tsx
@@ -1,4 +1,5 @@
 import useCartStore from "@/store/productStore";
+import cartIcon from "@/assets/images/icon-add-to-cart.svg";
 import { formatNumber } from "@/utils/currencyFormat";
 
 type ProductsItemPropsType = {
@@ -18,7 +19,9 @@ const ProductsItem = ({ product }: ProductsItemPropsType) => {
   const decreaseCartQuantity = useCartStore((state) => state.decreaseCartItem);
 
   return (
-    <div className="product__card">
+    <div
+      className={cartQuantity > 0 ? "product__card active" : "product__card"}
+    >
       <div className="product__card__top">
         <img
           className="card__img"
@@ -34,7 +37,8 @@ const ProductsItem = ({ product }: ProductsItemPropsType) => {
               addCartItem(product);
             }}
           >
-            add to the cart
+            <img src={cartIcon} alt="Cart Icon SVG" />
+            Add to the Cart
           </button>
         ) : (
           <div className="card__btn__flex">
@@ -45,7 +49,15 @@ const ProductsItem = ({ product }: ProductsItemPropsType) => {
                 decreaseCartQuantity(product.id);
               }}
             >
-              -
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                height="2"
+                fill="none"
+                viewBox="0 0 10 2"
+              >
+                <path fill="#fff" d="M0 .375h10v1.25H0V.375Z" />
+              </svg>
             </button>
             <p className="card--price">{cartQuantity}</p>
             <button
@@ -55,7 +67,18 @@ const ProductsItem = ({ product }: ProductsItemPropsType) => {
                 increaseCartQuantity(product.id);
               }}
             >
-              +
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                height="10"
+                fill="none"
+                viewBox="0 0 10 10"
+              >
+                <path
+                  fill="#fff"
+                  d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                />
+              </svg>
             </button>
           </div>
         )}
