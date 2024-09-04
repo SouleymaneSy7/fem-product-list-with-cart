d872e1be4e0ae97437202f932ac6718755c4a0ea
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-04 02:29:00 +0200

    refactor: refactor the ProductsItem component

--- src/components/ProductsItem.tsx
+++ src/components/ProductsItem.tsx
@@ -1,6 +1,12 @@
+import Button from "./Buttons";
 import useCartStore from "@/store/productStore";
-import cartIcon from "@/assets/images/icon-add-to-cart.svg";
+
 import { formatNumber } from "@/utils/currencyFormat";
+import {
+  AddToCartIcon,
+  DecrementIcon,
+  IncrementIcon,
+} from "./icons/Icons.component";
 
 type ProductsItemPropsType = {
   product: {
@@ -30,56 +36,36 @@ const ProductsItem = ({ product }: ProductsItemPropsType) => {
         />
 
         {cartQuantity === 0 ? (
-          <button
-            type="button"
+          <Button
             className="card--btn--add-to"
             onClick={() => {
               addCartItem(product);
             }}
           >
-            <img src={cartIcon} alt="Cart Icon SVG" />
+            <AddToCartIcon />
             Add to the Cart
-          </button>
+          </Button>
         ) : (
           <div className="card__btn__flex">
-            <button
-              type="button"
+            <Button
               className="card--btn--minus"
               onClick={() => {
                 decreaseCartQuantity(product.id);
               }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="10"
-                height="2"
-                fill="none"
-                viewBox="0 0 10 2"
-              >
-                <path fill="#fff" d="M0 .375h10v1.25H0V.375Z" />
-              </svg>
-            </button>
+              <DecrementIcon />
+            </Button>
+
             <p className="card--price">{cartQuantity}</p>
-            <button
-              type="button"
+
+            <Button
               className="card--btn--plus"
               onClick={() => {
                 increaseCartQuantity(product.id);
               }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="10"
-                height="10"
-                fill="none"
-                viewBox="0 0 10 10"
-              >
-                <path
-                  fill="#fff"
-                  d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
-                />
-              </svg>
-            </button>
+              <IncrementIcon />
+            </Button>
           </div>
         )}
       </div>
